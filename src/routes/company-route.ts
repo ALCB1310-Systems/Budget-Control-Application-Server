40a56ea4a42a6d7f6a5a1f65af0b5247ea667c37
Andrés Court <andres@andrescourt.com>
2022-11-12 17:02:42 -0500

    Feature: added field validation

--- src/routes/company-route.ts
+++ src/routes/company-route.ts
@@ -7,11 +7,15 @@ const router: Router = express.Router()
 router.post('/', async (req: Request, res: Response) => {
     if (req.body && Object.keys(req.body).length === 0 && Object.getPrototypeOf(req.body) === Object.prototype ) return res.status(400).json({detail: `Need to send user information`})
 
-    const { ruc, name, employees }: companyCreate = req.body
+    const { ruc, name, employees, email, password, fullname }: companyCreate = req.body
 
     if(!ruc) return res.status(400).json({detail: "The ruc field is required"})
     if(!name) return res.status(400).json({detail: "The name field is required"})
     if(!employees) return res.status(400).json({detail: "The employees field is required"})
+
+    if(!email) return res.status(400).json({detail: "The email field is required"})
+    if(!password) return res.status(400).json({detail: "The password field is required"})
+    if(!fullname) return res.status(400).json({detail: "The fullname field is required"})
     
     const createCompanyResult = await createCompany(req.body)
 
