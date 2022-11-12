aaf798fa85f45b03cf86487388b28f6c34e4b77b
Andrés Court <andres@andrescourt.com>
2022-11-11 22:51:43 -0500

    Testing: first test

---
+++ dist/tests/main.test.js
@@ -0,0 +1,6 @@
+"use strict";
+describe('My first TS test', () => {
+    it('should assert that true is true', () => {
+        expect(true).toBe(true);
+    });
+});
---
+++ tests/main.test.ts
@@ -0,0 +1,5 @@
+describe('My first TS test', () => { 
+    it('should assert that true is true', () => {
+        expect(true).toBe(true)
+    })
+ })
