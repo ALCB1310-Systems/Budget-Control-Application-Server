822b72d3eba57477a547e1734cbc0b7d9d012d3e
Andrés Court <andres@andrescourt.com>
2022-11-26 19:36:44 -0500

    cors functionality

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import express, { Express, Request, Response } from 'express'
 import swaggerUI from 'swagger-ui-express'
 import swaggerDocument from './swagger/swagger.json'
-
 import routes from './routes'
+import cors from 'cors'
+
+const allowedOrigins = '*'
+const options: cors.CorsOptions = {
+    origin: allowedOrigins
+}
 
 const app: Express = express()
-
+app.use(cors(options))
 app.use(express.json())
 app.use(routes)
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
